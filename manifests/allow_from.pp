0d67b9dcb407f0f8ce6d8f3978f54d4be6d71238
Andy Loftus <aloftus@gmail.com>
2019-09-05 12:20:58 -0500

    Add support for sssd_conf manipulation
    Update dependencies in metadata
    
    Add users & groups params
    
    PDK update from 1.10.0 to 1.12.0
    
    Add sshd
    
    Revert to puppetlabs-firewall
    
    Remove firewalld
    Rename allowed_subnets to trusted_subnets
    
    pdk update to 1.13.0
    
    Add augeasfacter for sssd_domains
    
    Fix typo (missing colon)
    
    Test augeasfacter
    
    Add notify's in sshd.
    
    Test augeasfacter using canned example.
    
    Add augeasversion_from_plugin
    
    Add augeasversion_from_plugin
    
    to differentiate from augeasversion which is already present
    outside of augeasfacter
    
    Test split regexp
    
    Implement use of custom fact sssd_domains
    
    pdk validate fixes
    
    Add empty defaults to allow_from

--- manifests/allow_from.pp
+++ manifests/allow_from.pp
@@ -5,9 +5,19 @@
 #   + Configures sshd_config with a Match directive and associated parameter
 #     settings
 #
+# @param users
+#   Type: Array
+#   Desc: list of users to allow (from hostlist)
+#   Note: If both "users" and "groups" are empty, error is raised.
+#
+# @param groups
+#   Type: Array
+#   Desc: list of groups to allow (from hostlist)
+#   Note: If both "users" and "groups" are empty, error is raised.
+#
 # @param hostlist
 #   Type: Array
-#   Desc: list of IPs or Hostnames that are allowed to ssh to this node
+#   Desc: list of IPs or Hostnames that (users/groups) are allowed to ssh from
 #
 # @param sshd_cfg_match_params
 #   Type: Hash
@@ -18,36 +28,45 @@
 #                                   }
 #
 # @example
-#   ssh::allow_from { 'comment':
-#       'hostlist'         => Array
-#       'sshd_cfg_match_params' => Hash
+#   ssh::allow_from { 'allow incoming ssh by users 1,2,3 from hosts X,Y,Z':
+#       'users'                 => Array,
+#       'groups'                => Array,
+#       'hostlist'              => Array,
+#       'sshd_cfg_match_params' => Hash,
 #   }
 define ssh::allow_from(
-    Array[ String ]         $pam_access_groups = [],
-    Array[ String ]         $pam_access_users = [],
-    Array[ String, 1 ]      $hostlist,
-    Hash[ String, Data, 1 ] $sshd_cfg_match_params,
+    Array[ String, 1 ]   $hostlist,
+    Array[ String ]      $users                  = [],
+    Array[ String ]      $groups                 = [],
+    Hash[ String, Data ] $sshd_cfg_match_params  = {},
 ) {
 
+    ### CHECK INPUT
+    # one of users or groups must not be empty
+    if empty( $users ) and empty( $groups ) {
+        fail( "'users' and 'groups' cannot both be empty" )
+    }
+
+
     ### ACCESS.CONF
-    # This sets up the pam access.conf file to allow ssh
-    $pam_access_groups.each |String $pam_access_group| { $hostlist.each |String $host| {
-        pam_access::entry { "Allow $pam_access_group ssh from $host":
-            group      => $pam_access_group,
+    ### This sets up the pam access.conf file to allow ssh
+    $groups.each |String $group| { $hostlist.each |String $host| {
+        pam_access::entry { "Allow ${group} ssh from ${host}":
+            group      => $group,
+            origin     => $host,
+            permission => '+',
+            position   => '-1',
+        }
+    }}
+    $users.each |String $user| { $hostlist.each |String $host| {
+        pam_access::entry { "Allow ${user} ssh from ${host}":
+            user       => $user,
             origin     => $host,
             permission => '+',
             position   => '-1',
         }
     }}
 
-    $pam_access_users.each |String $pam_access_user| { $hostlist.each |String $host| {
-        pam_access::entry { "Allow $pam_access_user ssh from $host":
-            user       => $pam_access_user,
-            origin     => $host,
-            permission => '+',
-            position   => '-1',
-        }
-    }}
 
     ### FIREWALL
     $hostlist.each | $host | {
@@ -69,7 +88,33 @@ define ssh::allow_from(
     }
 
 
-    ### SSHD
+    ### SSSD
+    # Requires custom fact 'sssd_domains'
+    # See: lib/augeasfacter/sssd_info.conf
+    # See also: https://github.com/woodsbw/augeasfacter
+    ###
+    # convert sssd domains from csv string to a puppet array
+    $domains = $facts['sssd_domains'].regsubst(/ +/, '', 'G').split(',')
+    $domains.each |$domain| {
+        if $users =~ Array[String,1] {
+            $csv = $users.join(',')
+            ::sssd::domain::array_append { "${name} users '${csv}' for sssd domain '${domain}'" :
+                domain  => $domain,
+                setting => 'simple_allow_users',
+                items   => $users,
+            }
+        }
+        if $groups =~ Array[String,1] {
+            $csv = $groups.join(',')
+            ::sssd::domain::array_append { "${name} groups '${csv}' for sssd domain '${domain}'" :
+                domain  => $domain,
+                setting => 'simple_allow_groups',
+                items   => $groups,
+            }
+        }
+    }
+
+    ### SSHD_CONFIG
     # Defaults
     $config_defaults = {
         'notify' => Service[ sshd ],
@@ -78,7 +123,21 @@ define ssh::allow_from(
         'position' => 'before first match'
     }
 
-    # Hostnames require "Match Host", IPs/CIDRs require "Match Address"
+    # Create cfg_match_params for Users and Groups
+    $user_params = $users ? {
+        Array[ String, 1 ] => { 'AllowUsers' => $users },
+        default            => {}
+    }
+    $group_params = $groups ? {
+        Array[ String, 1 ] => { 'AllowGroups' => $groups },
+        default            => {}
+    }
+
+    # Combine all cfg_match_params into a single hash
+    $cfg_match_params = $sshd_cfg_match_params + $user_params + $group_params
+
+    # Hostnames require "Match Host"
+    # IPs/CIDRs require "Match Address"
     # Create separate lists and make two separate match blocks in sshd_config
     # criteria will be either "Host" or "Address"
     # pattern will be the CSV string of hostnames or IPs
@@ -86,13 +145,14 @@ define ssh::allow_from(
     $name_list = $hostlist.filter | $elem | { $elem =~ /[a-zA-Z]/ }
     $ip_list   = $hostlist.filter | $elem | { $elem !~ /[a-zA-Z]/ }
     #associate the correct criteria with each list, filter empty lists
-    $data = { 'Host'    => $name_list,
-              'Address' => $ip_list,
-            }.filter | $criteria, $list | {
-                size( $list ) > 0
-            }
-    #loop through data creating a match block for each criteria-pattern
-    $data.each | $criteria, $list | {
+    $host_data = {
+        'Host'    => $name_list,
+        'Address' => $ip_list,
+    }.filter | $criteria, $list | {
+        size( $list ) > 0
+    }
+    #loop through host_data creating a match block for each criteria-pattern
+    $host_data.each | $criteria, $list | {
         $pattern = join( $list, ',' )
         $match_condition = "${criteria} ${pattern}"
 
@@ -105,7 +165,7 @@ define ssh::allow_from(
         }
 
         #add parameters to the match block
-        $sshd_cfg_match_params.each | $key, $val | {
+        $cfg_match_params.each | $key, $val | {
             sshd_config {
                 "${match_condition} ${key}" :
                     key       => $key,
